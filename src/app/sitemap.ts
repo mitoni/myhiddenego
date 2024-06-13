7ccbc2c8367876867fd1c2c2b7958698eb8c3106
michele-tonizzo <mt@moi.am>
2024-06-13 08:44:04 -0400

    fix sitemap url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,25 +3,25 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: `https://${process.env.VERCEL_URL}`,
+      url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: `https://${process.env.VERCEL_URL}/about-us`,
+      url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/about-us`,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: `https://${process.env.VERCEL_URL}/art-therapy`,
+      url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/art-therapy`,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: `https://${process.env.VERCEL_URL}/contacts`,
+      url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/contacts`,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
